e2fa24382212e8a84848980c71f00fd461523506
parthdixit15 <135131391+parthdixit15@users.noreply.github.com>
2024-04-09 00:44:31 +0530

    Add files via upload
    This webapp is made using streamlit. Model was trained with Multinomial Naive Bayes algorithm to do the classification and TFIDFvecetorizer to do the vectorization of text before feeding it to the model.

---
+++ webapp.py
@@ -0,0 +1,43 @@
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+import streamlit as st
+import pickle
+import os
+
+file_path = r"C:\Users\Asus\OneDrive\Desktop\reptile\sms_classifier\Model.pkl"
+file_path2= r"C:\Users\Asus\OneDrive\Desktop\reptile\sms_classifier\tfidfvectorizer.pkl"
+
+with open(file_path, 'rb') as file:  
+    Model = pickle.load(file)
+
+with open(file_path2, 'rb') as file2:  
+    feature_extraction = pickle.load(file2)
+
+def predictor(input_text):
+    
+    input_text_features=feature_extraction.transform([input_text])
+    prediction=Model.predict(input_text_features)
+
+    if(prediction[0]==1):
+        return 1
+    else:
+        return 0
+
+
+def main():
+    st.title('Message Classifier')
+    message = st.text_input('Enter a message:')
+    if st.button('Predict'):
+        prediction = predictor(message)
+        if(prediction==0):
+            st.header("Spam")
+        else:
+            st.header("Ham")    
+
+if __name__ == '__main__':
+    main()
+
+
+
